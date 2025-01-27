4fc50bba95cc9a06aaee8cab1c37ded964fbb9b0
Daniel <daniel.mrmnt@eduge.ch>
2025-01-27 16:07:08 +0100

    Create guess_the_flag.sql
    Ajout du fichier sql de la database

---
+++ Backend/guess_the_flag.sql
@@ -0,0 +1,32 @@
+-- Réinitialisation et création de la base de données
+DROP DATABASE IF EXISTS `guess_the_flag`;
+CREATE DATABASE `guess_the_flag`;
+
+USE `guess_the_flag`;
+
+-- Création des tables
+CREATE TABLE regions (
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE users (
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
+    `username` VARCHAR(50) NOT NULL,
+    `password` CHAR(64) NOT NULL,
+    `score` INT NOT NULL,
+    `idRegion` INT NOT NULL,
+    FOREIGN KEY (`idRegion`)
+    REFERENCES regions(`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+);
+
+-- Ajout des régions
+INSERT INTO regions (name) VALUES
+('Europe'),
+('Asie'),
+('Afrique'),
+('Amérique du Nord'),
+('Amérique du Sud'),
+('Océanie');
